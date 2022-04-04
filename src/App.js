928d399f36b1c03f299ca27c9382fd30fe80c23e
Reagan <reaganrandolph1015@gmail.com>
2022-04-04 11:41:30 -0500

    Added route links to other pages to home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <h1>APP</h1>
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Shopping Cart</Link>
+        </li>
+      </ul>
     </div>
   );
 };
