15405490925f3f963f516ad0da50e30f7c399892
Reagan <reaganrandolph1015@gmail.com>
2022-04-05 17:15:43 -0500

    Created card-container grid for displaying shoes in Shop route

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,6 +5,7 @@ import TS from '../imgs/travis.jpg';
 import AM from '../imgs/Airmax.webp';
 import AF from '../imgs/Airforce.png';
 import NB from '../imgs/Newbalance.png';
+import UB from '../imgs/Ultraboost.png';
 
 const Cards = () => {
   let cardArray = [];
@@ -17,13 +18,16 @@ const Cards = () => {
     </div>
   );
 
+  // Variables for shoe cards
   const common = createCard(CP, 'Common Projects', '305', uniqid());
-  const trav = createCard(TS, "Travis Scott's", '2500', uniqid());
+  const trav = createCard(TS, 'Air Jordan 1 Travis Scott', '2500', uniqid());
   const airmax = createCard(AM, 'Nike Airmax', '100', uniqid());
   const airforce = createCard(AF, 'Nike Airforce 1', '100', uniqid());
   const newBalance = createCard(NB, 'New Balance 550', '120', uniqid());
+  const ultraboost = createCard(UB, 'Ultraboost 22', '200', uniqid());
 
-  cardArray.push(common, trav, airmax, airforce, newBalance);
+  // add created shoe cards to existing array
+  cardArray.push(common, trav, airmax, airforce, newBalance, ultraboost);
 
   return cardArray;
 };
